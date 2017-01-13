14302ed5abd80d97df8be633a1c5ae9e1a91398b
Diego Fernando Pinto <diepinto30@gmail.com>
2017-01-13 17:46:24 -0500

    Nuevo avance ya con text

---
+++ canva_prueba/Assets/Txt360.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Txt360 : MonoBehaviour {
+	public string txtFile = "dato";
+	string txtContents;
+
+
+	// Use this for initialization
+	void Start () {
+		TextAsset txtAssets = (TextAsset)Resources.Load(txtFile);
+		txtContents = txtAssets.text;
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		GUILayout.Label (txtContents);
+	}
+}
